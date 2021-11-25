770c8cfb486f1309bbc89639895157dd929f3ce4
N8Dev <n8dev.cs@gmail.com>
2021-11-25 13:52:21 -0500

    changed project root directory.

---
+++ Assets/-Source-/Scripts/Runtime/Conversion/ConvertedImage.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace N8SpriteConverter
+{
+    [DisallowMultipleComponent, RequireComponent(typeof(RawImage))]
+    public sealed class ConvertedImage : MonoBehaviour
+    {
+        [field: SerializeField]
+        public Vector2 MaximumSize { get; [UsedImplicitly] private set; }
+        
+        public RectTransform RectTransform { get; private set; }
+        public RawImage RawImage { get; private set; }
+        public Texture2D Texture { get; set; }
+
+        private void Awake()
+        {
+            RectTransform = GetComponent<RectTransform>();
+            RawImage = GetComponent<RawImage>();
+        }
+    }
+}
